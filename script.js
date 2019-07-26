a66acf99a7073ac53c227930fb4589ba024451d3
lucas munhoz <lubarlettamunhoz@gmail.com>
2019-07-26 17:37:06 -0300

    Adicionar condicoes

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
 let quadrados = document.querySelectorAll(".casa");
-let cores = ["red", "blue"];
+let resposta = document.querySelector("h3");
 
 
-function gerarX(){
-    this.style.backgroundColor = cores[0];
-}
-
-function gerarO(){
-    this.style.backgroundColor = cores[1]; 
-}
 let count = 0;
 
 
 
 function gerarJogada(){
-    if (count % 2 == 0){
-        this.innerHTML = "X";
-        count++;
-    } else {
-        this.innerHTML= "O";
-        count++;
-    } 
+    if(this.innerHTML == ""){
+        if (count % 2 == 0){
+            this.innerHTML = "X";
+            count++;
+            verificarResultado();
+        } else {
+            this.innerHTML= "O";
+            count++;
+            verificarResultado();
+        } 
+
+    }
+    
 }
-
-
 
 for(let quadrado of quadrados){
     quadrado.onclick = gerarJogada;
 }
+function verificarResultado (){
+    if(quadrados[0].innerHTML !=="" && quadrados[0].innerHTML === quadrados[1].innerHTML && quadrados[1].innerHTML=== quadrados[2].innerHTML){
+        resposta.innerHTML = "voce venceu";
+    } 
+    else if(quadrados[3].innerHTML !=="" && quadrados[3].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML=== quadrados[5].innerHTML){
+        resposta.innerHTML = "voce venceu";
+    } 
+    else if(quadrados[6].innerHTML !=="" && quadrados[6].innerHTML === quadrados[7].innerHTML && quadrados[7].innerHTML=== quadrados[8].innerHTML){
+        resposta.innerHTML = "voce venceu";
+    } 
+    else if(quadrados[0].innerHTML !=="" && quadrados[0].innerHTML === quadrados[3].innerHTML && quadrados[3].innerHTML=== quadrados[6].innerHTML){
+        resposta.innerHTML = "voce venceu";
+    } 
+    else if(quadrados[1].innerHTML !=="" && quadrados[1].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML=== quadrados[7].innerHTML){
+        resposta.innerHTML = "voce venceu";
+    } 
+    else if(quadrados[2].innerHTML !=="" && quadrados[2].innerHTML === quadrados[5].innerHTML && quadrados[5].innerHTML=== quadrados[8].innerHTML){
+        resposta.innerHTML = "voce venceu";
+    } 
+    else if(quadrados[0].innerHTML !=="" && quadrados[0].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML=== quadrados[8].innerHTML){
+        resposta.innerHTML = "voce venceu";
+    } 
+    else if(quadrados[2].innerHTML !=="" && quadrados[2].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML=== quadrados[6].innerHTML){
+        resposta.innerHTML = "voce venceu";
+    } 
+    else if(count==9){
+        resposta.innerHTML="deu velha";
+    }
 
-
+    
+}
 
