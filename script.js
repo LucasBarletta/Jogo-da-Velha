0e4aa0706ed999797876eafc4cd980f803c4d596
lucas munhoz <lubarlettamunhoz@gmail.com>
2019-07-29 15:14:09 -0300

    Adicionar background

--- script.js
+++ script.js
@@ -25,28 +25,28 @@ for(let quadrado of quadrados){
 }
 function verificarResultado (){
     if(quadrados[0].innerHTML !=="" && quadrados[0].innerHTML === quadrados[1].innerHTML && quadrados[1].innerHTML=== quadrados[2].innerHTML){
-        resposta.innerHTML = "voce venceu";
+        resposta.innerHTML = "Você venceu";
     } 
     else if(quadrados[3].innerHTML !=="" && quadrados[3].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML=== quadrados[5].innerHTML){
-        resposta.innerHTML = "voce venceu";
+        resposta.innerHTML = "Você venceu";
     } 
     else if(quadrados[6].innerHTML !=="" && quadrados[6].innerHTML === quadrados[7].innerHTML && quadrados[7].innerHTML=== quadrados[8].innerHTML){
-        resposta.innerHTML = "voce venceu";
+        resposta.innerHTML = "Você venceu";
     } 
     else if(quadrados[0].innerHTML !=="" && quadrados[0].innerHTML === quadrados[3].innerHTML && quadrados[3].innerHTML=== quadrados[6].innerHTML){
-        resposta.innerHTML = "voce venceu";
+        resposta.innerHTML = "Você venceu";
     } 
     else if(quadrados[1].innerHTML !=="" && quadrados[1].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML=== quadrados[7].innerHTML){
-        resposta.innerHTML = "voce venceu";
+        resposta.innerHTML = "Você venceu";
     } 
     else if(quadrados[2].innerHTML !=="" && quadrados[2].innerHTML === quadrados[5].innerHTML && quadrados[5].innerHTML=== quadrados[8].innerHTML){
-        resposta.innerHTML = "voce venceu";
+        resposta.innerHTML = "Você venceu";
     } 
     else if(quadrados[0].innerHTML !=="" && quadrados[0].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML=== quadrados[8].innerHTML){
-        resposta.innerHTML = "voce venceu";
+        resposta.innerHTML = "Você venceu";
     } 
     else if(quadrados[2].innerHTML !=="" && quadrados[2].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML=== quadrados[6].innerHTML){
-        resposta.innerHTML = "voce venceu";
+        resposta.innerHTML = "Você venceu";
     } 
     else if(count==9){
         resposta.innerHTML="deu velha";
